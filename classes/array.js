8201ae754c4d515abe32dff4559e88883069403f
Tanzil <mtanzilislam1997@gmail.com>
2022-10-21 15:57:55 +0100

    fin

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,19 @@
 // Your code here
+// npm test test/array-spec.js
+
+
+// Your code here
+
+Array.prototype.isEqual = function(array) {
+
+    for (let i = 0; i < this.length; i++) {
+      let el = this[i];
+      let arrayEl = array[i];
+
+      if (el !== arrayEl) {
+        return false;
+      }
+    }
+
+    return true;
+  }
